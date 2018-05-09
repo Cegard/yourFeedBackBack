0dfe3a286a13bd15352270114cdaec40c626a788
achacon <achacon@endava.com>
2018-05-09 14:41:07 -0500

    fixed something.

--- technical-challenge/src/main/java/com/backend/technicalchallenge/persistance/EvaluatedUserRepository.java
+++ technical-challenge/src/main/java/com/backend/technicalchallenge/persistance/EvaluatedUserRepository.java
@@ -1,12 +1,14 @@
 package com.backend.technicalchallenge.persistance;
 
 import com.backend.technicalchallenge.model.evaluation.EvaluatedUser;
+import com.backend.technicalchallenge.model.event.Event;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EvaluatedUserRepository extends PagingAndSortingRepository<EvaluatedUser, Long> {
 
-    List<EvaluatedUser> findAllByEvent_Id(Long eventId);
+    List<EvaluatedUser> findAllByEvent(Optional<Event> eventId);
 
 }
