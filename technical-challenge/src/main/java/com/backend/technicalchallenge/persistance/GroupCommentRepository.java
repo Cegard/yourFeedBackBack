7ee4484f4adff237a2214a565e448eabf7fdbaa2
achacon <achacon@endava.com>
2018-05-16 15:52:18 -0500

    changes.

--- technical-challenge/src/main/java/com/backend/technicalchallenge/persistance/GroupCommentRepository.java
+++ technical-challenge/src/main/java/com/backend/technicalchallenge/persistance/GroupCommentRepository.java
@@ -3,6 +3,7 @@ package com.backend.technicalchallenge.persistance;
 import com.backend.technicalchallenge.model.evaluation.GroupComment;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +12,8 @@ public interface GroupCommentRepository  extends PagingAndSortingRepository<Grou
 
     Optional<GroupComment> findById(Long id);
     List<GroupComment> findAll();
-    List<GroupComment> findByGroupApp_IdAndAndEvaluation_Id(Long idGroupApp, Long idEvaluation);
+    @Query(value = "SELECT gcom.* FROM group_comment gcom where gcom.evaluation_id = :idEvaluation and gcom.group_app_id = :idGroupApp", nativeQuery = true)
+    List<GroupComment> findByGroupApp_IdAndAndEvaluation_Id(@Param("idGroupApp")Long idGroupApp, @Param("idEvaluation") Long idEvaluation);
 
 
 }
